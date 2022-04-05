d0940e021db3aba0d8b2461d180daf8542397038
unknown <oluwatobiloba8624@gmail.com>
2022-04-05 23:09:15 +0100

    built this simple app

---
+++ src/container/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Calculator from '../components/Calculator';
+
+function App() {
+  return (
+    <div className='body'>
+      <h1>Celsius - Fahrenheit Calculator</h1>
+      <Calculator />
+    </div>
+    
+  );
+}
+
+export default App;
